f73c0e48329683cd7dc4fb50bf787ae7e5715574
FlorianFusseder-Desktop <florianfueder@yahoo.de>
2016-10-15 21:54:23 +0200

    Changed a few things because of first usage of Reshaper

---
+++ NAS.Updater/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using MVVM_Vorlage;
+
+namespace NAS.Updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public class MainWindow : Window
+    {
+        private MainWindowViewModel mainWindow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainWindow = new MainWindowViewModel();
+            DataContext = mainWindow;
+        }
+    }
+}
