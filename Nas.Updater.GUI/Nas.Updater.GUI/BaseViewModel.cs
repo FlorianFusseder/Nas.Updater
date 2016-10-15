e0d936163887a76db88d3b6fbc7d169238c4851b
FlorianFusseder-Desktop <florianfueder@yahoo.de>
2016-10-15 22:50:58 +0200

    Added a Dummy GUI to the Project

---
+++ Nas.Updater.GUI/Nas.Updater.GUI/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Nas.Updater.GUI
+{
+    abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
